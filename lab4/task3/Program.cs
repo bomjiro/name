6a41780ff41edb3caab2dcad0b2e25399635dec4
bomjiro <112935570+bomjiro@users.noreply.github.com>
2022-11-25 22:27:30 +0300

    Add files via upload

---
+++ lab4/task3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Debugger
+{
+    class Program
+    {
+        static void Main()
+        {
+            var typeDebugger = new TypeDebugger();
+
+            typeDebugger.Debugger("lab4", 10, 10.10);
+        }
+    }
+}
